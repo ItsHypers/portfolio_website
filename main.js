a3e697d9ce84705db57d157ace6f11177f565e61
Ryan <ryanathomas013@gmail.com>
2022-12-28 16:22:50 +0000

    portfolio start

--- main.js
+++ main.js
@@ -104,6 +104,43 @@ $(function () {
   $(".mdi-menu").click(function () {
     $(".link-wrap").toggleClass("visible");
   });
+  // GALLERY
+  $("#gallery").mixItUp({});
+
+  function mixClear() {
+    setTimeout(function () {
+      $("#gallery").removeClass("waypoint");
+    }, 2000);
+  }
+
+  // SCROLL ANIMATIONS
+  function onScrollInit(items, elemTrigger) {
+    var offset = $(window).height() / 1.6;
+    items.each(function () {
+      var elem = $(this),
+        animationClass = elem.attr("data-animation"),
+        animationDelay = elem.attr("data-delay");
+
+      elem.css({
+        "-webkit-animation-delay": animationDelay,
+        "-moz-animation-delay": animationDelay,
+        "animation-delay": animationDelay,
+      });
+
+      var trigger = elemTrigger ? trigger : elem;
+
+      trigger.waypoint(
+        function () {
+          elem.addClass("animated").addClass(animationClass);
+          if (elem.get(0).id === "gallery") mixClear(); //OPTIONAL
+        },
+        {
+          triggerOnce: true,
+          offset: offset,
+        }
+      );
+    });
+  }
 
   // CONTACT FORM
   $("#contact-form").submit(function (e) {
